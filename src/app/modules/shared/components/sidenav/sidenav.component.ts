33b067e73cb5008c14172e34e720b888607fba9c
Angel Taborda <angelt9707@gmail.com>
2023-06-23 14:05:58 -0500

    sidenav

--- src/app/modules/shared/components/sidenav/sidenav.component.ts
+++ src/app/modules/shared/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,7 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
-  constructor() { }
+  mobileQuery : MediaQueryList;
+
+  menuNav = [
+    {name : "Home", route: "home", icon:"home"},
+    {name : "Categorias", route: "category", icon:"category"},
+    {name : "Productos", route: "product", icon:"production_quantity_limits"}
+  ]
+
+  constructor(media: MediaMatcher) {
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+   }
 
   ngOnInit(): void {
   }
